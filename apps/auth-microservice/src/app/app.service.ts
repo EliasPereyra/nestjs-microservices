5c8bc73f950ee4ed6c4a58e58be8bad5b6f37f04
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-01-12 18:16:33 -0300

    Use the user repository

--- apps/auth-microservice/src/app/app.service.ts
+++ apps/auth-microservice/src/app/app.service.ts
@@ -1,8 +1,16 @@
+import { CreateUserDto } from '@nestjs-microservices/shared';
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { User } from '@nestjs-microservices/shared/entities';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  constructor(private readonly userRepository: UserRepository) {}
+  createUser(user: CreateUserDto) {
+    this.userRepository.save(user);
+  }
+
+  getUser(id: number): User {
+    return this.userRepository.findOne(id);
   }
 }
