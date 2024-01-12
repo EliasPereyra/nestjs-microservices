0f5ba5334c1714d5d20807b0613ecadd24ae8cfe
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-01-12 18:19:58 -0300

    Create the controller for listening the events

--- apps/auth-microservice/src/app/app.controller.ts
+++ apps/auth-microservice/src/app/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, ValidationPipe } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { CreateUserDto } from '@nestjs-microservices/shared';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @EventPattern('create_user')
+  handleUserCreate(@Payload(ValidationPipe) data: CreateUserDto) {
+    this.appService.createUser(data);
   }
 }
