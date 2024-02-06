081c7ecdf7fc215081b1f6650515327c2cdfd4d8
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-02-05 23:25:45 -0300

    Create a payment service

--- apps/api-gateway/src/payment/payment.service.ts
+++ apps/api-gateway/src/payment/payment.service.ts
@@ -5,7 +5,7 @@ import { ClientKafka } from '@nestjs/microservices';
 @Injectable()
 export class PaymentService {
   constructor(
-    @Inject('PAYMENT_SERVICE') private readonly paymentClient: ClientKafka
+    @Inject('PAYMENT_MICROSERVICE') private readonly paymentClient: ClientKafka
   ) {}
 
   makePayment(makePaymentDto: MakePaymentDTO) {
