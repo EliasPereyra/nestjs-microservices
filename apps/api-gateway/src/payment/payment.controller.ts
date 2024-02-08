7e6af6cb4734beca207a5fe8a5afb5508e87a812
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-02-07 23:39:05 -0300

    Create a payment controller

--- apps/api-gateway/src/payment/payment.controller.ts
+++ apps/api-gateway/src/payment/payment.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { MakePaymentDTO } from '@nestjs-microservices/shared';
 
-@Controller('payment')
+@Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
