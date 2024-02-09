83499a8b59241641448bc03895ca0956e1ea773c
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-02-08 23:05:48 -0300

    Create an auth microservice controller

--- apps/auth-microservice/src/app/app.controller.ts
+++ apps/auth-microservice/src/app/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, ValidationPipe } from '@nestjs/common';
+import { Controller, ParseIntPipe, ValidationPipe } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateUserDto } from '@nestjs-microservices/shared';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @MessagePattern('get_user')
+  handleGetUser(@Payload('userId', ParseIntPipe) userId: number) {
+    return this.appService.getUser(userId);
+  }
 
   @EventPattern('create_user')
   handleUserCreate(@Payload(ValidationPipe) data: CreateUserDto) {
